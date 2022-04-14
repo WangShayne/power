004cebfb9ea27252c1ff00c6e5300ce91e433d48
wangshuai <wangshuai@njjialong.cn>
2022-04-14 17:57:04 +0800

    ✨ feat(ColumnList): add ColumnList component

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <ColumnList :list="testData"/>  
+</template>
+
+<script setup lang="ts">
+import ColumnList, { ColumnListProps } from "./components/ColumnList.vue";
+const testData: ColumnListProps[] = [
+  {
+    id: 0,
+    title: 'test1',
+    description: 'this is a test description',
+    avatar: 'https://avatars.githubusercontent.com/u/17644818?v=4'
+  },
+  {
+    id: 2,
+    title: 'test1',
+    description: 'this is a test description',
+    avatar: 'https://avatars.githubusercontent.com/u/17644818?v=4'
+  },
+  {
+    id: 3,
+    title: 'test1',
+    description: 'this is a test description',
+    avatar: 'https://avatars.githubusercontent.com/u/17644818?v=4'
+  },
+  {
+    id: 4,
+    title: 'test1',
+    description: 'this is a test description',
+    avatar: 'https://avatars.githubusercontent.com/u/17644818?v=4'
+  }
+]
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,50 @@
+<template>
+    <ul  class="grid grid-flow-row grid-cols-3 grid-rows-3 gap-4 p-2">
+        <li v-for="item in list" :key="item.id">
+            <figure class="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
+                <img class="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" :src="item.avatar" alt="" width="384" height="512">
+                <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+                    <blockquote>
+                    <p class="text-lg font-semibold">
+                       {{item.description}}
+                    </p>
+                    </blockquote>
+                    <figcaption class="font-medium">
+                    <div class="text-cyan-600">
+                        {{item.title}}
+                    </div>
+                    <div class="text-gray-500">
+                        <a href="#">进入专栏</a>
+                    </div>
+                    </figcaption>
+                </div>
+            </figure>
+        </li>
+        
+    </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface ColumnListProps{
+    id: number;
+    title: string;
+    avatar: string;
+    description: string;
+}
+
+export default defineComponent({
+    name: 'ColumnList',
+    props: {
+        list: {
+            type: Array as PropType<ColumnListProps[]>,
+            requird: true
+        }
+    },
+    setup(props) {
+        return {
+            ...props
+        }
+    },
+})
+</script>
